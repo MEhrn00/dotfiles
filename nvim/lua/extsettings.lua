a9407f1b4365dd2f976a62b6e7069a11581efabf
Matt Ehrnschwender <matthewe2020@gmail.com>
2023-10-21 13:59:07 -0400

    Refactor to remove legacy things

--- nvim/lua/extsettings.lua
+++ nvim/lua/extsettings.lua
@@ -30,95 +30,173 @@ require('telescope').setup{
   },
 }
 
--- Git settings
-local neogit = require('neogit')
-neogit.setup{
-  kind = "vsplit",
+-- Add more specific filetype detections
+local function yaml_ftdetect(path, bufnr)
+  if vim.fn.glob(vim.fn.getcwd() .. "/**/ansible.cfg") ~= "" then
+    return "yaml.ansible"
+  end
+  return "yaml"
+end
+
+vim.filetype.add({
+  extension = {
+    yaml = yaml_ftdetect,
+    yml = yaml_ftdetect,
+  }
+})
+
+-- NeoGit settings
+require('neogit').setup{
+  kind = "auto",
   integrations = {
+    telescope = true,
     diffview = true
   }
 }
 
+-- GitSigns settings
+require('gitsigns').setup{
+  current_line_blame = true,
+  current_line_blame_opts = {
+    delay = 500,
+  }
+}
+
+-- Neotree settings
+ require("neo-tree").setup({
+   default_component_configs = {
+     git_status = {
+       symbols = {
+         untracked = "U",
+         ignored = "I",
+         unstaged = "M",
+         staged = "S",
+         conflict = "C"
+       }
+     }
+   },
+
+   source_selector = {
+     winbar = true,
+
+     sources = {
+       { source = 'filesystem' },
+       { source = 'git_status' },
+     }
+   }
+ })
+
 -- Dev icons
 require('nvim-web-devicons').setup()
 
--- Setup completion
-local lsp = require('lsp-zero')
-lsp.preset('recommended')
+-- Setup LSP
+local lsp_zero = require('lsp-zero')
+lsp_zero.on_attach(function(_, bufnr)
+  local opts = { buffer = bufnr, remap = false }
 
-lsp.ensure_installed({
+  -- Command keybinds for nvim lsp such as documentation and goto implementation
+  vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
+  vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, opts)
+  vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, opts)
+
+  vim.keymap.set('n', '<C-k>', function() vim.lsp.buf.signature_help() end, opts)
+  keymap('n', '<leader>r', ':Telescope lsp_references<CR>', { silent = true })
+  vim.keymap.set('n', 'ge', function() vim.diagnostic.goto_next() end, opts)
+  vim.keymap.set('n', 'gE', function() vim.diagnostic.goto_prev() end, opts)
+  keymap('n', '<leader>e', ':Telescope diagnostics<CR>', { silent = true })
+  vim.keymap.set('n', 'ga', function() vim.lsp.buf.code_action() end, opts)
+
+  lsp_zero.default_keymaps({buffer = bufnr})
+end)
+
+lsp_zero.format_on_save({
+  format_opts = {
+    async = true,
+    timeout_ms = 10000,
+  },
+
+  servers = {
+    ['rust_analyzer'] = {'rust'},
+    ['gopls'] = {'go'},
+    ['clangd'] = {'c', 'cpp'},
+    ['terraformls'] = {'terraform'},
+  }
+})
+
+require('mason').setup({}) -- Mason for handling language servers
+
+require('mason-lspconfig').setup({
+  ensure_installed = {
     'rust_analyzer',
     'gopls',
     'clangd',
+  },
+
+  handlers = {
+    lsp_zero.default_setup,
+
+    rust_analyzer = function()
+      require('lspconfig').rust_analyzer.setup({
+        settings = {
+          checkOnSave = {
+            command = "clippy"
+          },
+
+          cargo = {
+            features = "all"
+          }
+        }
+      })
+    end,
+
+    clangd = function ()
+      require('lspconfig').clangd.setup({
+        settings = {
+          command = {
+            'clangd',
+            '-clang-tidy',
+            '-clang-tidy-checks=-*,clang-analyzer-*,modernize-*,readability-*',
+          }
+        }
+      })
+    end
+  },
 })
 
--- Setup LSP initialization status
-require('fidget').setup{}
 
--- Setup Rust completion
-lsp.configure('rust_analyzer', {
-    settings = {
-        ["rust-analyzer"] = {
-            checkOnSave = {
-                command = "clippy"
-            },
-
-            cargo = {
-                features = "all"
-            }
-        },
-    }
-})
-
---lsp.configure('clangd', {
---  settings = {
---    cmd = {
---      'clangd',
---      '-clang-tidy',
---      '-clang-tidy-checks=-*,clang-analyzer-*,modernize-*,readability-*'
---    }
---  }
---})
-
--- Set LSP preferences to not include sign icons
-lsp.set_preferences({
-    sign_icons = { }
-})
-
--- Set code completion keybinds
-local cmp = require('cmp')
+local cmp = require('cmp') -- Completion behavior
 local cmp_select = { behavior = cmp.SelectBehavior.Select }
-local cmp_mappings = lsp.defaults.cmp_mappings({
+
+-- Snippets load
+require("luasnip.loaders.from_vscode").lazy_load()
+
+cmp.setup({
+  mapping = cmp.mapping.preset.insert({
+    ['<CR>'] = nil,
     ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
     ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-d>'] = cmp.mapping.scroll_docs(4),
+    ['<C-e>'] = cmp.mapping.abort(),
     ['<Tab>'] = cmp.mapping.confirm({ select = true }),
     ['<C-Space>'] = cmp.mapping.complete(),
+  }),
+
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body)
+    end,
+  },
+
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+    { name = 'buffer' },
+    { name = 'path' },
+  })
 })
 
-cmp_mappings['<CR>'] = nil
 
-lsp.setup_nvim_cmp({
-    mapping = cmp_mappings
-})
-
-lsp.on_attach(function(client, bufnr)
-    local opts = { buffer = bufnr, remap = false }
-
-    -- Command keybinds for nvim lsp such as documentation and goto implementation
-    vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
-    vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, opts)
-    vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, opts)
-
-    vim.keymap.set('n', '<C-k>', function() vim.lsp.buf.signature_help() end, opts)
-    keymap('n', '<leader>r', ':Telescope lsp_references<CR>', { silent = true })
-    vim.keymap.set('n', 'ge', function() vim.diagnostic.goto_next() end, opts)
-    vim.keymap.set('n', 'gE', function() vim.diagnostic.goto_prev() end, opts)
-    keymap('n', '<leader>e', ':Telescope diagnostics<CR>', { silent = true })
-    vim.keymap.set('n', 'ga', function() vim.lsp.buf.code_action() end, opts)
-end)
-
--- Finish LSP setup
-lsp.setup()
 vim.diagnostic.config({
     virtual_text = true,
     signs = true,
@@ -127,6 +205,9 @@ vim.diagnostic.config({
     severity_sort = false,
     float = true,
 })
+
+-- Setup LSP status display
+require('fidget').setup{}
 
 -- Setup status line
 require('lualine').setup{
@@ -138,7 +219,11 @@ require('lualine').setup{
     lualine_a = {'mode'},
     lualine_b = {'branch', 'diff', 'diagnostics'},
     lualine_c = {'filename'},
-    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_x = {
+      'encoding',
+      'fileformat',
+      'filetype'
+    },
     lualine_y = {'progress'},
     lualine_z = {'location'},
   },
@@ -202,78 +287,6 @@ end
 
 dapui.setup()
 
--- Setup format on save for rust, go and c++
-require('formatter').setup({
-    logging = false,
-    filetype = {
-      go = {
-          function()
-              return {
-                  exe = "gofmt",
-                  stdin = true
-          }
-          end
-      },
-
-      rust = {
-          function()
-              return {
-                  exe = "rustfmt",
-                  args = {
-                      "--emit=stdout",
-                      "--edition=2021",
-                      "--color=never",
-                  },
-                  stdin = true
-              }
-          end
-      },
-
-      cpp = {
-          function()
-              return {
-                  exe = "clang-format",
-                  args = {'-i', "-style=file"},
-                  stdin = false,
-                  cwd = vim.fn.expand('%:p:h')
-              }
-          end
-      },
-
-      c = {
-          function()
-              return {
-                  exe = "clang-format",
-                  args = {'-i', '-style=file'},
-                  stdin = false,
-                  cwd = vim.fn.expand('%:p:h')
-              }
-          end
-      },
-
-      python = {
-          function()
-              return {
-                  exe = "black",
-                  args = {"--no-color"},
-                  stdin = false,
-          }
-          end
-      },
-
-      tf = {
-          function()
-              return {
-                  exe = "terraform",
-                  args = {"fmt", "-no-color", "-"},
-                  stdin = true,
-              }
-          end
-      },
-
-    }
-})
-
 -- Treesitter
 require'nvim-treesitter.configs'.setup{
   auto_install = true,
@@ -288,14 +301,6 @@ require'treesitter-context'.setup{}
 
 -- Buffer info
 require('bufferline').setup()
-
--- Format on save write hook
-vim.cmd [[
-augroup Format
-    autocmd!
-    autocmd BufWritePost *.rs,*.go,*.cpp,*.c,*.h,*.py,*.tf silent! FormatWrite
-augroup END
-]]
 
 -- Build task runner
 require('overseer').setup()
