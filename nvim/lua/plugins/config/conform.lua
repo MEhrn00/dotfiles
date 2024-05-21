42a134e920a9b290caa2ff4fa293161edaac305f
Matt Ehrnschwender <matthewe2020@gmail.com>
2024-05-21 12:51:19 -0400

    Formatting

--- nvim/lua/plugins/config/conform.lua
+++ nvim/lua/plugins/config/conform.lua
@@ -1,26 +1,28 @@
 return {
-  "stevearc/conform.nvim",
-  event = { "BufWritePre" },
-  cmd = { "ConformInfo" },
+	"stevearc/conform.nvim",
+	event = { "BufWritePre" },
+	cmd = { "ConformInfo" },
 
-  opts = {
-    formatters_by_ft = {
-      lua = { "stylua" },
-      go = { "goimports", "gofmt" },
-      python = { "isort", "black" },
-    },
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" },
+			go = { "goimports", "gofmt" },
+			python = { "isort", "black" },
+			cpp = { "clang-format" },
+			c = { "clang-format" },
+		},
 
-    formatters = {
-      rustfmt = {
-        options = {
-          default_edition = "2021",
-        },
-      },
-    },
+		formatters = {
+			rustfmt = {
+				options = {
+					default_edition = "2021",
+				},
+			},
+		},
 
-    format_on_save = {
-      timeout_ms = 500,
-      lsp_fallback = true,
-    },
-  }
+		format_on_save = {
+			timeout_ms = 500,
+			lsp_fallback = true,
+		},
+	},
 }
