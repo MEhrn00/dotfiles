30b45e935d000182bf42f74a8b2a7da3644f34fd
Matt Ehrnschwender <matthewe2020@gmail.com>
2024-05-30 15:07:59 -0400

    Add treesitter default parsers

--- nvim/lua/plugins/config/treesitter.lua
+++ nvim/lua/plugins/config/treesitter.lua
@@ -1,12 +1,15 @@
 return {
-  'nvim-treesitter/nvim-treesitter',
-  build = ':TSUpdate',
-  lazy = false,
-  opts = {
-    ensure_installed = { 'c', 'rust', 'go', 'lua' },
-    auto_install = true,
-  },
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	lazy = false,
+	opts = {
+		ensure_installed = { "c", "rust", "go", "vim", "vimdoc", "lua" },
+		auto_install = true,
+		highlight = {
+			enable = true,
+		},
+	},
 
-  main = 'nvim-treesitter.configs',
-  config = true,
+	main = "nvim-treesitter.configs",
+	config = true,
 }
