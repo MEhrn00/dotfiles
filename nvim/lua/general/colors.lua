5abee9531fee6ba815b47a62ce243120b667d29e
Matt Ehrnschwender <matthewe2020@gmail.com>
2024-05-30 15:07:13 -0400

    Update colors

--- nvim/lua/general/colors.lua
+++ nvim/lua/general/colors.lua
@@ -1,9 +1,18 @@
 local M = {}
 
 function M.setup(scheme)
-  -- Set the background to dark and allow gui colors in the terminal
-  vim.opt.bg = 'dark'
-  vim.opt.termguicolors = true
+	-- Set the background to dark and allow gui colors in the terminal
+	vim.opt.bg = "dark"
+	vim.opt.termguicolors = true
+
+	-- Set the column line color to gray
+	vim.api.nvim_set_hl(0, "ColorColumn", { bg = "gray" })
+	vim.api.nvim_create_autocmd({ "ColorScheme" }, {
+		pattern = "*",
+		callback = function()
+			vim.api.nvim_set_hl(0, "ColorColumn", { bg = "gray" })
+		end,
+	})
 end
 
 return M
--- nvim/lua/plugins/config/colors.lua
+++ nvim/lua/plugins/config/colors.lua
@@ -1,8 +1,23 @@
 return {
-  'Mofiqul/vscode.nvim',
-  lazy = false,
-  priority = 1000,
-  config = function()
-    vim.cmd.colorscheme('vscode')
-  end,
+	"Mofiqul/vscode.nvim",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		vim.cmd.colorscheme("vscode")
+
+		-- Restore the gray column line
+		vim.api.nvim_set_hl(0, "ColorColumn", { bg = "gray" })
+		vim.api.nvim_create_autocmd({ "ColorScheme" }, {
+			pattern = "*",
+			callback = function()
+				vim.api.nvim_set_hl(0, "ColorColumn", { bg = "gray" })
+			end,
+		})
+
+		-- Make the background transparent if not in a GUI
+		if not vim.g.neovide and not (vim.fn.has("gui") == 1) then
+			vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+			vim.api.nvim_set_hl(0, "NonText", { bg = "none" })
+		end
+	end,
 }
