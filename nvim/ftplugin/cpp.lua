e1d9bfd1432c8952f840507c3e3bbd4b29a37ebe
Matt Ehrnschwender <matthewe2020@gmail.com>
2024-06-06 16:54:33 -0400

    Add cmake build keybind

--- nvim/ftplugin/cpp.lua
+++ nvim/ftplugin/cpp.lua
@@ -1,1 +1,18 @@
 vim.g.c_no_curly_error = 1
+
+local keymaps = require("general.keymaps")
+
+local fp = io.open(vim.fn.getcwd() .. "/CMakeLists.txt", "r")
+if fp ~= nil then
+	keymaps.add({
+		mode = "n",
+		keys = "<leader>b",
+		action = ":CMakeBuild<CR>",
+		desc = "Build CMake project",
+		opts = {
+			buffer = true,
+		},
+	})
+
+	fp:close()
+end
