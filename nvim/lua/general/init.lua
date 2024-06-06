b69baaf2f8fed8464a0cf2a329fcdf07889d5390
Matt Ehrnschwender <matthewe2020@gmail.com>
2024-06-06 16:52:01 -0400

    Formatting

--- nvim/lua/general/init.lua
+++ nvim/lua/general/init.lua
@@ -1,28 +1,27 @@
 local M = {}
 
 function M.setup()
-  require('general.options').setup()
-  require('general.keymaps').setup()
+	require("general.options").setup()
+	require("general.keymaps").setup()
 
-  -- require('general.colors').setup()
-  -- require('general.statusline').setup()
+	-- require('general.statusline').setup()
 
-  if vim.fn.has('win32') == 1 then
-    require('general.windows').setup()
-  elseif vim.fn.has('unix') == 1 then
-    require('general.linux').setup()
-  end
+	if vim.fn.has("win32") == 1 then
+		require("general.windows").setup()
+	elseif vim.fn.has("unix") == 1 then
+		require("general.linux").setup()
+	end
 
-  if vim.g.neovide then
-    require('general.neovide').setup()
-  end
+	if vim.g.neovide then
+		require("general.neovide").setup()
+	end
 
-  if vim.g.neovide or vim.fn.has('gui') then
-    require('general.gui').setup()
-  end
+	if vim.g.neovide or vim.fn.has("gui") then
+		require("general.gui").setup()
+	end
 
-  require('general.ctags').setup()
-  require('general.commenttoggle').setup()
+	require("general.ctags").setup()
+	require("general.commenttoggle").setup()
 end
 
 return M
