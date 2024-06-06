973a3cd1d4e02e89f8e0eb536a26aa671f0813dc
Matt Ehrnschwender <matthewe2020@gmail.com>
2024-06-06 16:51:45 -0400

    Fix colorscheme

--- nvim/lua/plugins/config/colors.lua
+++ nvim/lua/plugins/config/colors.lua
@@ -3,23 +3,6 @@ return {
 	lazy = false,
 	priority = 1000,
 	config = function()
-		vim.cmd.colorscheme("vscode")
-
-		-- Restore the gray column line
-		vim.api.nvim_set_hl(0, "ColorColumn", { bg = "gray" })
-		vim.api.nvim_create_autocmd({ "ColorScheme" }, {
-			pattern = "*",
-			callback = function()
-				vim.api.nvim_set_hl(0, "ColorColumn", { bg = "gray" })
-			end,
-		})
-
-		-- Make the background transparent if not in a GUI
-		if not vim.g.neovide and not (vim.fn.has("gui") == 1) then
-			vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-			vim.api.nvim_set_hl(0, "NonText", { bg = "none" })
-			vim.api.nvim_set_hl(0, "LineNr", { fg = "gray", bg = "none" })
-			vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
-		end
+		require("general.colors").setup({ colorscheme = "vscode" })
 	end,
 }
