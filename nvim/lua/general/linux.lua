5005b50fa6d9f7dfc2c531c5c42ebd3241645f4b
Matt Ehrnschwender <matthewe2020@gmail.com>
2024-06-06 16:52:38 -0400

    Remove unused settings

--- nvim/lua/general/linux.lua
+++ nvim/lua/general/linux.lua
@@ -1,11 +1,8 @@
 local M = {}
 
 -- Define the GUI font for the platform
-M.guifont = 'Source Code Pro'
+M.guifont = "Source Code Pro"
 
-function M.setup()
-  -- Add zshrc configuration to shell commands
-  vim.opt.shellcmdflag = '-ic'
-end
+function M.setup() end
 
 return M
