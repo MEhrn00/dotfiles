61ae5c681e49b7ba50bcb8c5fed495f9174a0603
Matt Ehrnschwender <matthewe2020@gmail.com>
2024-08-10 13:06:08 -0400

    Add overseer makeprg command

--- nvim/lua/plugins/modules/overseer.lua
+++ nvim/lua/plugins/modules/overseer.lua
@@ -35,5 +35,25 @@ return {
 		require("overseer").setup(opts)
 		local keymaps = require("utils.keymaps")
 		keymaps.apply(keybinds)
+
+		vim.api.nvim_create_user_command("Make", function(params)
+			-- Insert args at the '$*' in the makeprg
+			local cmd, num_subs = vim.o.makeprg:gsub("%$%*", params.args)
+			if num_subs == 0 then
+				cmd = cmd .. " " .. params.args
+			end
+			local task = require("overseer").new_task({
+				cmd = vim.fn.expandcmd(cmd),
+				components = {
+					{ "on_output_quickfix", open = not params.bang, open_height = 8 },
+					"default",
+				},
+			})
+			task:start()
+		end, {
+			desc = "Run makeprg as an Overseer task",
+			nargs = "*",
+			bang = true,
+		})
 	end,
 }
