e87cdb7ee16282089b5e5530eb2a82aba440bd25
Matt Ehrnschwender <matthewe2020@gmail.com>
2024-09-24 13:07:37 -0400

    Remove text width

--- nvim/ftplugin/rust.lua
+++ nvim/ftplugin/rust.lua
@@ -1,4 +1,3 @@
 -- Set cursor column and text width to 80 characters
-vim.opt_local.colorcolumn = '80'
-vim.opt_local.textwidth = 80
-vim.cmd.compiler('cargo')
+vim.opt_local.colorcolumn = "80"
+vim.cmd.compiler("cargo")
