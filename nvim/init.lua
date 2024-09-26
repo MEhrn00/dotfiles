2df22893889e4b3795fabc6313834dc402860202
Matt Ehrnschwender <matthewe2020@gmail.com>
2024-09-26 12:22:09 -0400

    Refactor config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,12 +1,1 @@
--- General no-dependency modules
-require("general").setup({
-	statusline = {
-		disabled = true,
-	},
-	commenttoggle = {
-		disabled = true,
-	},
-})
-
--- Plugin modules
-require("plugins").setup()
+require("general").setup()
