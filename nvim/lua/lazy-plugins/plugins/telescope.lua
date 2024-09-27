1fcf2398ef1d9688687f66639338070e06fcd6c9
Matt Ehrnschwender <matthewe2020@gmail.com>
2024-09-26 21:29:52 -0400

    compiler plugins

--- nvim/lua/lazy-plugins/plugins/telescope.lua
+++ nvim/lua/lazy-plugins/plugins/telescope.lua
@@ -37,45 +37,49 @@ return {
 	config = function(_, opts)
 		require("telescope").setup(opts)
 		local builtin = require("telescope.builtin")
-		local keymap = require("utils.keymaps")
+
 		require("telescope").load_extension("yaml_schema")
 
-		keymap.add({
-			mode = "n",
-			keys = "<c-p>",
-			action = builtin.find_files,
-			desc = "Find files",
-		})
-
-		keymap.add({
-			mode = "n",
-			keys = "<leader>s",
-			action = builtin.live_grep,
-			desc = "Search for text",
-		})
-
-		keymap.add({
-			mode = "v",
-			keys = "<leader>s",
-			action = function()
-				local selectedtext = getVisualSelection()
-				builtin.live_grep({ default_text = selectedtext })
-			end,
-			desc = "Search for selected word",
-		})
-
-		keymap.add({
-			mode = "n",
-			keys = "<leader>r",
-			action = builtin.lsp_references,
-			desc = "LSP references",
-		})
-
-		keymap.add({
-			mode = "n",
-			keys = "<leader>;",
-			action = builtin.buffers,
-			desc = "List currently open buffers",
+		require("utils.keymaps").apply({
+			{
+				mode = "n",
+				keys = "<c-p>",
+				action = builtin.find_files,
+				desc = "Find files",
+			},
+			{
+				mode = "n",
+				keys = "<leader>s",
+				action = builtin.live_grep,
+				desc = "Search for text",
+			},
+			{
+				mode = "v",
+				keys = "<leader>s",
+				action = function()
+					local selectedtext = getVisualSelection()
+					builtin.live_grep({ default_text = selectedtext })
+				end,
+				desc = "Search for selected word",
+			},
+			{
+				mode = "n",
+				keys = "<leader>r",
+				action = builtin.lsp_references,
+				desc = "LSP references",
+			},
+			{
+				mode = "n",
+				keys = "<leader>;",
+				action = builtin.buffers,
+				desc = "List currently open buffers",
+			},
+			{
+				mode = "n",
+				keys = "<leader>b",
+				action = ":make",
+				desc = "Run makeprg",
+			},
 		})
 	end,
 }
