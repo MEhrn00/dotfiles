379428dfcb711509beb5ed2dc42c404a7f6aa310
Matt Ehrnschwender <matthewe2020@gmail.com>
2024-09-29 21:56:19 -0400

    Update bettermake

--- nvim/lua/general/bettermake.lua
+++ nvim/lua/general/bettermake.lua
@@ -1,1 +1,22 @@
-return require("custom-plugins.bettermake")
+local M = {}
+
+function M.setup(opts)
+	require("custom-plugins.bettermake").setup(opts)
+	require("utils.keymaps").apply({
+		{
+			mode = "n",
+			keys = "<leader>bc",
+			action = ":BetterMakeCompile<CR>",
+			desc = "Compile project",
+		},
+
+		{
+			mode = "n",
+			keys = "<leader>bb",
+			action = ":BetterMakeRecompile<CR>",
+			desc = "Recompile project",
+		},
+	})
+end
+
+return M
