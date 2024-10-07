020486489da4da731c9f870c1bd3d7bb43523e99
Matt Ehrnschwender <matthewe2020@gmail.com>
2024-10-07 14:37:27 -0400

    Zsh updates

--- zsh/zsh/config/options.zsh
+++ zsh/zsh/config/options.zsh
@@ -10,14 +10,7 @@ setopt share_history
 if [[ $(tput colors 2>/dev/null) == "256" ]]; then
     COLORTERM="truecolor"
 
-    # Colored man pages
-    LESS_TERMCAP_mb=$'\E[01;32m'
-    LESS_TERMCAP_md=$'\E[01;32m'
-    LESS_TERMCAP_me=$'\E[0m'
-    LESS_TERMCAP_se=$'\E[0m'
-    LESS_TERMCAP_so=$'\E[01;47;34m'
-    LESS_TERMCAP_ue=$'\E[0m'
-    LESS_TERMCAP_us=$'\E[01;36m'
+    export MANPAGER='less -R --use-color -Du51 -Dd83 -DP4.7 -DE4.7 -DS4.7'
 fi
 
 # GPG tty
@@ -35,4 +28,3 @@ select-word-style bash
 
 # Use emacs keybinds
 set -o emacs
-
