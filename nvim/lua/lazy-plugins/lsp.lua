3a1c2e3c5dd06e836448a6ba8df362015c510fc7
Matt Ehrnschwender <matthewe2020@gmail.com>
2024-10-14 12:39:46 -0400

    Refactor lsp setup

--- nvim/lua/lazy-plugins/lsp.lua
+++ nvim/lua/lazy-plugins/lsp.lua
@@ -24,161 +24,324 @@ return {
 	{
 		"neovim/nvim-lspconfig",
 		dependencies = {
-			"hrsh7th/nvim-cmp",
-			"hrsh7th/cmp-nvim-lsp",
-			"saadparwaiz1/cmp_luasnip",
-			"L3MON4D3/LuaSnip",
-			"onsails/lspkind.nvim",
+			{
+				"hrsh7th/nvim-cmp",
+				dependencies = {
+					"onsails/lspkind.nvim",
+					"hrsh7th/cmp-nvim-lsp",
+					"hrsh7th/cmp-nvim-lsp-signature-help",
+					"saadparwaiz1/cmp_luasnip",
+					"L3MON4D3/LuaSnip",
+				},
+				opts = function()
+					local cmp = require("cmp")
+					local lspkind = require("lspkind")
+					local luasnip = require("luasnip")
+					return {
+						snippet = {
+							expand = function(args) luasnip.lsp_expand(args.body) end,
+						},
+
+						mapping = cmp.mapping.preset.insert({
+							["<CR>"] = nil,
+							["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
+							["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+							["<C-u>"] = cmp.mapping.scroll_docs(-4),
+							["<C-d>"] = cmp.mapping.scroll_docs(4),
+							["<C-f>"] = cmp.mapping.scroll_docs(1),
+							["<C-b>"] = cmp.mapping.scroll_docs(-1),
+							["<C-e>"] = cmp.mapping.abort(),
+							["<Tab>"] = cmp.mapping.confirm({ select = true }),
+							["<C-Space>"] = cmp.mapping.complete(),
+						}),
+
+						sources = cmp.config.sources({
+							{ name = "nvim_lsp" },
+							{ name = "luasnip" },
+							{ name = "nvim_lsp_signature_help" },
+						}),
+
+						enabled = function()
+							local context = require("cmp.config.context")
+							return not context.in_treesitter_capture("comment") and not context.in_syntax_group("Comment")
+						end,
+
+						formatting = {
+							format = lspkind.cmp_format({
+								mode = "symbol_text",
+								maxwidth = 50,
+								ellipsis_char = "...",
+								show_labelDetails = true,
+							}),
+						},
+
+						experimental = {
+							ghost_text = true,
+						},
+					}
+				end,
+			},
 			"williamboman/mason.nvim",
 			"williamboman/mason-lspconfig",
 			"b0o/schemastore.nvim",
 		},
 
-		config = function()
-			require("mason").setup()
-			require("mason-lspconfig").setup()
+		opts = function()
+			local schemastore = require("schemastore")
+			return {
+				servers = {
+					lua_ls = {
+						settings = {
+							Lua = {
+								runtime = { version = "LuaJIT" },
+								workspace = {
+									checkThirdParty = false,
+									library = {
+										"${3rd}/luv/library",
+										unpack(vim.api.nvim_get_runtime_file("", true)),
+									},
+								},
 
-			local cmp = require("cmp")
-			cmp.setup({
-				snippet = {
-					expand = function(args)
-						require("luasnip").lsp_expand(args.body)
-					end,
-				},
-
-				mapping = cmp.mapping.preset.insert({
-					["<CR>"] = nil,
-					["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
-					["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
-					["<C-u>"] = cmp.mapping.scroll_docs(-4),
-					["<C-d>"] = cmp.mapping.scroll_docs(4),
-					["<C-f>"] = cmp.mapping.scroll_docs(1),
-					["<C-b>"] = cmp.mapping.scroll_docs(-1),
-					["<C-e>"] = cmp.mapping.abort(),
-					["<Tab>"] = cmp.mapping.confirm({ select = true }),
-					["<C-Space>"] = cmp.mapping.complete(),
-				}),
-
-				sources = cmp.config.sources({
-					{ name = "nvim_lsp" },
-					{ name = "luasnip" },
-				}),
-
-				enabled = function()
-					local context = require("cmp.config.context")
-					return not context.in_treesitter_capture("comment") and not context.in_syntax_group("Comment")
-				end,
-
-				formatting = {
-					format = require("lspkind").cmp_format({
-						mode = "symbol",
-						maxwidth = 50,
-						ellipsis_char = "...",
-						show_labelDetails = true,
-					}),
-				},
-
-				experimental = {
-					ghost_text = true,
-				}
-
-			})
-
-			local servers = {
-				lua_ls = {
-					settings = {
-						Lua = {
-							runtime = { version = "LuaJIT" },
-							workspace = {
-								checkThirdParty = false,
-								library = {
-									"${3rd}/luv/library",
-									unpack(vim.api.nvim_get_runtime_file("", true)),
+								completion = {
+									callSnippet = "Replace",
 								},
 							},
+						},
+					},
 
-							completion = {
-								callSnippet = "Replace",
+					rust_analyzer = {
+						settings = {
+							['rust-analyzer'] = {
+								check = {
+									command = "clippy",
+								},
+								lens = {
+									enable = true,
+								},
+								semanticHighlighting = {
+									operator = {
+										enable = true,
+										specialization = {
+											enable = true,
+										},
+									},
+									punctuation = {
+										enable = true,
+										separate = {
+											macro = {
+												bang = true,
+											},
+										},
+										specialization = {
+											enable = true,
+										},
+									},
+								},
+								completion = {
+									fullFunctionSignatures = {
+										enable = true,
+									},
+								},
 							},
 						}
 					},
-				},
 
-				pylsp = {
-					settings = {
-						pylsp = {
-							plugins = {
-								ruff = {
-									enabled = true,
-								},
-								pyflakes = {
-									enabled = false,
-								},
-							},
-						},
-					},
-				},
-
-				yamlls = require("yaml-companion").setup({
-					lspconfig = {
+					pylsp = {
 						settings = {
-							yaml = {
-								valudate = true,
-								schemaStore = {
-									enabled = false,
-									url = "",
+							pylsp = {
+								plugins = {
+									ruff = {
+										enabled = true,
+									},
+									pyflakes = {
+										enabled = false,
+									},
 								},
-								schemas = require("schemastore").yaml.schemas(),
 							},
 						},
 					},
-				}),
 
-				jsonls = {
-					settings = {
-						json = {
-							schemas = require("schemastore").json.schemas(),
-							validate = { enable = true },
+					yamlls = require("yaml-companion").setup({
+						lspconfig = {
+							settings = {
+								yaml = {
+									valudate = true,
+									schemaStore = {
+										enabled = false,
+										url = "",
+									},
+									schemas = schemastore.yaml.schemas(),
+								},
+							},
+						},
+					}),
+
+					jsonls = {
+						settings = {
+							json = {
+								schemas = schemastore.json.schemas(),
+								validate = { enable = true },
+							},
 						},
 					},
 				},
+
+				diagnostics = {
+					underline = true,
+					virtual_text = {
+						severity = vim.diagnostic.severity.ERROR,
+						spacing = 4,
+						source = "if_many",
+						hl_mode = "combine",
+					},
+					update_in_insert = true,
+					severity_sort = true,
+					signs = {
+						text = {
+							[vim.diagnostic.severity.ERROR] = "󰅚 ",
+							[vim.diagnostic.severity.WARN] = "󰀪 ",
+							[vim.diagnostic.severity.HINT] = "󰌶 ",
+							[vim.diagnostic.severity.INFO] = " ",
+						},
+						texthl = {
+							[vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
+							[vim.diagnostic.severity.WARN] = "DiagnosticSignWarn",
+							[vim.diagnostic.severity.HINT] = "DiagnosticSignHint",
+							[vim.diagnostic.severity.INFO] = "DiagnosticSignInfo",
+						},
+						numhl = {
+							[vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
+							[vim.diagnostic.severity.WARN] = "DiagnosticSignWarn",
+							[vim.diagnostic.severity.HINT] = "DiagnosticSignHint",
+							[vim.diagnostic.severity.INFO] = "DiagnosticSignInfo",
+						},
+					},
+				},
+
+				inlay_hints = {
+					enabled = true,
+					exclude = { "rust_analyzer" },
+				},
+
+				codelens = {
+					enabled = true,
+				},
+
+				document_highlight = {
+					enabled = true,
+				},
+
+				capabilities = {
+					workspace = {
+						fileOperations = {
+							didRename = true,
+							willRename = true,
+						},
+					}
+				},
+
+				keys = {
+					{"n", "gd", vim.lsp.buf.definition, { desc = "Go to definition", silent = true }},
+					{"n", "gD", vim.lsp.buf.declaration, { desc = "Go to declaration", silent = true }},
+					{"n", "gi", vim.lsp.buf.implementation, { desc = "Go to implementation", silent = true }},
+					{"n", "go", vim.lsp.buf.type_definition, { desc = "Go to type definition", silent = true }},
+					{"n", "<C-k>", vim.lsp.buf.signature_help, { desc = "Display signature help", silent = true }},
+					{"n", "crn", vim.lsp.buf.rename, { desc = "Rename symbol", silent = true }},
+					{"n", "ga", vim.lsp.buf.code_action, { desc = "Select code action", silent = true }},
+				},
+
+				setup = {},
 			}
+		end,
+
+		config = function(_, opts)
+			require("mason").setup()
+			require("mason-lspconfig").setup()
+
+			local cmp_lsp = require("cmp_nvim_lsp")
+			local capabilities = vim.tbl_deep_extend(
+				"force",
+				{},
+				vim.lsp.protocol.make_client_capabilities(),
+				cmp_lsp.default_capabilities(),
+				opts.capabilities or {}
+			)
 
 			require("mason-lspconfig").setup_handlers({
 				function(server_name)
-					local opts = vim.tbl_deep_extend("force", {}, servers[server_name] or {})
-					opts.capabilities = vim.tbl_deep_extend("force", {},
-						require("cmp_nvim_lsp").default_capabilities(),
-						opts.capabilities or {})
+					local server_opts = vim.tbl_deep_extend("force",
+						{ capabilities = vim.deepcopy(capabilities) },
+						opts.servers[server_name] or {}
+					)
 
-					require("lspconfig")[server_name].setup(opts)
+					if server_opts.enabled == false then
+						return
+					end
+
+					require("lspconfig")[server_name].setup(server_opts)
 				end,
 			})
 
-			local signs = { Error = "󰅚 ", Warn = "󰀪 ", Hint = "󰌶 ", Info = " " }
-			for type, icon in pairs(signs) do
-				local hl = "DiagnosticSign" .. type
-				vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-			end
-
-			vim.diagnostic.config({
-				virtual_text = true,
-				signs = true,
-				underline = true,
-				update_in_insert = true,
-				severity_sort = false,
-			})
+			vim.diagnostic.config(opts.diagnostics)
 
 			vim.api.nvim_create_autocmd("LspAttach", {
-				callback = function(event)
+				callback = function(args)
 					local map = vim.keymap.set
-					map("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition", silent = true, buffer = event.buf })
-					map("n", "gD", vim.lsp.buf.declaration, { desc = "Go to declaration", silent = true, buffer = event.buf })
-					map("n", "gi", vim.lsp.buf.implementation, { desc = "Go to implementation", silent = true, buffer = event.buf })
-					map("n", "go", vim.lsp.buf.type_definition, { desc = "Go to type definition", silent = true, buffer = event.buf })
-					map("n", "<C-k>", vim.lsp.buf.signature_help, { desc = "Display signature help", silent = true, buffer = event.buf })
-					map("n", "crn", vim.lsp.buf.rename, { desc = "Rename symbol", silent = true, buffer = event.buf })
-					map("n", "ga", vim.lsp.buf.code_action, { desc = "Select code action", silent = true, buffer = event.buf })
+					for _, keybind in ipairs(opts.keys) do
+						map(
+							keybind[1],
+							keybind[2],
+							keybind[3],
+							vim.tbl_deep_extend("keep", { buffer = args.buf }, keybind[4] or {})
+						)
+					end
+
+					vim.opt_local.updatetime = 500
+
+					local client = vim.lsp.get_client_by_id(args.data.client_id)
+					if client ~= nil then
+						if opts.document_highlight.enabled then
+							if not vim.iter(opts.document_highlight.exclude or {}):any(function(v) return v == client.name end) then
+								if client.server_capabilities.documentHighlightProvider then
+									vim.api.nvim_create_augroup("lsp_document_highlight", { clear = false })
+
+									vim.api.nvim_clear_autocmds { buffer = args.buf, group = "lsp_document_highlight" }
+									vim.api.nvim_create_autocmd({"CursorHold", "CursorHoldI"}, {
+											callback = vim.lsp.buf.document_highlight,
+											buffer = args.buf,
+											group = "lsp_document_highlight",
+											desc = "Document Highlight",
+									})
+
+									vim.api.nvim_create_autocmd({"CursorMoved", "CursorMovedI"}, {
+											callback = vim.lsp.buf.clear_references,
+											buffer = args.buf,
+											group = "lsp_document_highlight",
+											desc = "Clear All the References",
+									})
+								end
+							end
+						end
+
+						if opts.inlay_hints.enabled then
+							if not vim.iter(opts.inlay_hints.exclude or {}):any(function(v) return v == client.name end) then
+								if client.server_capabilities.inlayHintProvider then
+									vim.lsp.inlay_hint.enable(true, { bufnr = args.buf})
+								end
+							end
+						end
+
+						if opts.codelens.enabled then
+							if not vim.iter(opts.codelens.exclude or {}):any(function(v) return v == client.name end) then
+								if client.server_capabilities.codeLensProvider then
+									vim.api.nvim_create_autocmd({"BufEnter", "CursorHold", "InsertLeave"}, {
+										buffer = args.buf,
+										callback = vim.lsp.codelens.refresh,
+									})
+								end
+							end
+						end
+					end
 				end,
 			})
 		end,
