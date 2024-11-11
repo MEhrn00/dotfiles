6725c31d40f191c6dedd9e5a39ff877669ae18e3
Matt Ehrnschwender <matthewe2020@gmail.com>
2024-11-11 14:17:34 -0500

    Add tmux alias and remove extraneous whitespace
    Create an alias for tmux aliasing `t` to tmux. Remove extra whitespace
    between alias definitions

--- zsh/zsh/config/aliases.zsh
+++ zsh/zsh/config/aliases.zsh
@@ -1,17 +1,15 @@
 alias ls="ls --color=auto"
-
 alias ll="ls -alF"
 alias la="ls -A"
 alias l="ls -CF"
 
 alias k="kubectl"
-
 alias grep="grep --color=auto"
-
 alias ec="emacsclient"
-
 alias neogit='nvim -c "Neogit kind=replace"'
+alias t="tmux"
 
+# In WSL
 if [ -f /proc/sys/fs/binfmt_misc/WSLInterop ]; then
     alias code='/mnt/c/Users/matth/AppData/Local/Programs/Microsoft\ VS\ Code/bin/code'
     alias cmd.exe=/mnt/c/Windows/System32/cmd.exe
