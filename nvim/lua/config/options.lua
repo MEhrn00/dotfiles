38795a3259f3ad796fceb3f6aa88ffa22cd3565d
Matt Ehrnschwender <matthewe2020@gmail.com>
2024-12-29 15:44:39 -0500

    Adjust quickfix list open size to 15 lines

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -88,7 +88,7 @@ vim.api.nvim_create_user_command("Copew", function(opts)
 	if opts.args ~= nil and opts.args:len() > 0 then
 		vim.cmd('botright copen ' .. opts.args)
 	else
-		vim.cmd('botright copen')
+		vim.cmd('botright copen 15')
 	end
 end, { nargs = '*' })
 
@@ -96,6 +96,6 @@ vim.api.nvim_create_user_command("Lopew", function(opts)
 	if opts.args ~= nil and opts.args:len() > 0 then
 		vim.cmd('botright lopen ' .. opts.args)
 	else
-		vim.cmd('botright lopen')
+		vim.cmd('botright lopen 15')
 	end
 end, { nargs = '*' })
