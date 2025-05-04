c15914ac16bc4161be83586d9e93596d0b094593
Matt Ehrnschwender <matthewe2020@gmail.com>
2025-05-04 15:29:06 -0400

    Change fish variable scoping

--- fish/conf.d/aws.fish
+++ fish/conf.d/aws.fish
@@ -1,4 +1,4 @@
-set -q AWS_CONFIG_FILE; or set -Ux AWS_CONFIG_FILE "$XDG_CONFIG_HOME/aws/config"
-set -q AWS_SHARED_CREDENTIALS_FILE; or set -Ux AWS_SHARED_CREDENTIALS_FILE "$XDG_CONFIG_HOME/aws/credentials"
-set -q AWS_DATA_PATH; or set -Ux AWS_DATA_PATH "$XDG_CONFIG_HOME/aws/models"
-set -q AWS_PAGER; or set -Ux AWS_PAGER "less -F -X"
+set -q AWS_CONFIG_FILE; or set -gx AWS_CONFIG_FILE "$XDG_CONFIG_HOME/aws/config"
+set -q AWS_SHARED_CREDENTIALS_FILE; or set -gx AWS_SHARED_CREDENTIALS_FILE "$XDG_CONFIG_HOME/aws/credentials"
+set -q AWS_DATA_PATH; or set -gx AWS_DATA_PATH "$XDG_CONFIG_HOME/aws/models"
+set -q AWS_PAGER; or set -gx AWS_PAGER "less -F -X"
--- fish/conf.d/env.fish
+++ fish/conf.d/env.fish
@@ -1,21 +1,21 @@
-set -q EDITOR; or set -Ux EDITOR nvim
+set -q EDITOR; or set -gx EDITOR nvim
 set -q LESS; or set -Ux LESS -R
 set -q PAGER; or set -Ux PAGER 'less -F'
-set -q MANGPAGER; or set -Ux MANPAGER less
+set -q MANGPAGER; or set -gx MANPAGER less
 set -q GROFF_NO_SGR; or set -Ux GROFF_NO_SGR 1
 
-set -q JAVA_HOME; or test -e /etc/alternatives/java_sdk_openjdk; and set -Ux JAVA_HOME /etc/alternatives/java_sdk_openjdk
+set -q JAVA_HOME; or test -e /etc/alternatives/java_sdk_openjdk; and set -gx JAVA_HOME /etc/alternatives/java_sdk_openjdk
 
-set -q SSH_AUTH_SOCK; or set -Ux SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/ssh-agent.socket"
+set -q SSH_AUTH_SOCK; or set -gx SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/ssh-agent.socket"
 
-set -q NVM_DIR; or set -Ux NVM_DIR "$XDG_CONFIG_HOME/nvm"
+set -q NVM_DIR; or set -gx NVM_DIR "$XDG_CONFIG_HOME/nvm"
 
 if status is-interactive
     if test -x /usr/local/bin/nvim; or test -x /usr/bin/nvim
-        set -Ux MANPAGER 'nvim +Man!'
+        set -gx MANPAGER 'nvim +Man!'
     end
 
     if tput colors 2>/dev/null | grep -q '256'
-        set -Ux PAGER 'less -R --use-color -Du51 -Dd83 -DP4.7 -DE4.7 -DS4.7'
+        set -gx PAGER 'less -R --use-color -Du51 -Dd83 -DP4.7 -DE4.7 -DS4.7'
     end
 end
