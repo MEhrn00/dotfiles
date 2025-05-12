e65e2575f35888540f1877e43d25369fbc93b4de
Matt Ehrnschwender <matthewe2020@gmail.com>
2025-05-12 17:08:06 -0400

    Fixup lua ls

--- nvim/lua/lazy-plugins/lsp.lua
+++ nvim/lua/lazy-plugins/lsp.lua
@@ -98,10 +98,8 @@ return {
 							Lua = {
 								runtime = { version = "LuaJIT" },
 								workspace = {
-									checkThirdParty = false,
 									library = {
-										"${3rd}/luv/library",
-										unpack(vim.api.nvim_get_runtime_file("", true)),
+										table.unpack(vim.api.nvim_get_runtime_file("", true)),
 									},
 								},
 
