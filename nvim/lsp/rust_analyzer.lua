0ba8226c8fe8f988bcc04202e07376899c28163e
Matt Ehrnschwender <matthewe2020@gmail.com>
2025-05-16 14:01:47 -0400

    Formatting

--- nvim/lsp/rust_analyzer.lua
+++ nvim/lsp/rust_analyzer.lua
@@ -1,36 +1,36 @@
 return {
-  settings = {
-    ["rust-analyzer"] = {
-      check = {
-        command = "clippy",
-      },
-      lens = {
-        enable = true,
-      },
-      semanticHighlighting = {
-        operator = {
-          enable = true,
-          specialization = {
-            enable = true,
-          },
-        },
-        punctuation = {
-          enable = true,
-          separate = {
-            macro = {
-              bang = true,
-            },
-          },
-          specialization = {
-            enable = true,
-          },
-        },
-      },
-      completion = {
-        fullFunctionSignatures = {
-          enable = true,
-        },
-      },
-    }
-  }
+	settings = {
+		["rust-analyzer"] = {
+			check = {
+				command = "clippy",
+			},
+			lens = {
+				enable = true,
+			},
+			semanticHighlighting = {
+				operator = {
+					enable = true,
+					specialization = {
+						enable = true,
+					},
+				},
+				punctuation = {
+					enable = true,
+					separate = {
+						macro = {
+							bang = true,
+						},
+					},
+					specialization = {
+						enable = true,
+					},
+				},
+			},
+			completion = {
+				fullFunctionSignatures = {
+					enable = true,
+				},
+			},
+		},
+	},
 }
