17d8c7c58f57a55eec27e44393115f4a0c6ecda9
Matt Ehrnschwender <matthewe2020@gmail.com>
2025-06-14 15:46:32 -0400

    Switch to .luarc.json for neovim config

--- nvim/lua/config/lsp/lua_ls.lua
+++ nvim/lua/config/lsp/lua_ls.lua
@@ -1,20 +1,5 @@
 return {
 	settings = {
-		Lua = {
-			runtime = {
-				version = "LuaJIT",
-				path = {
-					"lua/?.lua",
-					"lua/?/init.lua",
-				},
-			},
-			workspace = {
-				checkThirdParty = false,
-				library = {
-					vim.env.VIMRUNTIME,
-					"${3rd}/luv/library",
-				},
-			},
-		},
+		Lua = {}
 	},
 }
