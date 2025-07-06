4b326dc60546f9317103296bf8dc51dc88ea0049
Matt Ehrnschwender <matthewe2020@gmail.com>
2025-07-06 15:34:38 -0400

    Fix powershell theme

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -7,6 +7,11 @@ Set-PSReadLineKeyHandler -Chord Ctrl+w -Function BackwardKillWord
 
 Set-Alias -Name k -Value kubectl.exe
 
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/powerline.omp.json" | Invoke-Expression
+$poshThemePath = Join-Path -Path (Split-Path "$PROFILE" -Parent) -ChildPath powerline-osc99.omp.json
+if (Test-Path -Path $poshThemePath) {
+    oh-my-posh init pwsh --config "$poshThemePath" | Invoke-Expression
+} else {
+    oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/powerline.omp.json" | Invoke-Expression
+}
 
 Import-Module -Name Microsoft.WinGet.CommandNotFound
